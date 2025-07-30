d6421cc9a53270c4c42ffd764eba3e81619ac962
mechseiko <mechseiko@gmail.com>
2025-07-30 23:51:22 +0100

    -t

--- amtapro_frontend/src/App.jsx
+++ amtapro_frontend/src/App.jsx
@@ -18,6 +18,9 @@ function App() {
             <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
           </div>
         </div>
+        <h1 class="text-3xl font-bold underline">
+          Hello world!
+        </h1>
       </div>
     </>
   )
