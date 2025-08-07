ea61cbe6ec06d78dc97dfa01ce2a0efe3f6844b5
mechseiko <mechseiko@gmail.com>
2025-08-07 10:00:32 +0100

    Learning Tailwind css

--- amtapro_frontend/src/App.jsx
+++ amtapro_frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+// https://www.figma.com/design/zhShyVtHRyxfAubEfFNrbN/Untitled?node-id=1-3&t=nSV5CvmHh4N6JSnm-1
 
 function App() {
   
@@ -8,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element></Route>
+        <Route path="/"></Route>
       </Routes>
     </BrowserRouter>
   )
