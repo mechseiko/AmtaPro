fec9a5f9df40c3d06f95d5f8a49f49930a174400
mechseiko <mechseiko@gmail.com>
2025-08-10 01:30:46 +0100

    Deployed to vercel for demo viewing

--- amtapro_frontend/src/App.jsx
+++ amtapro_frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
-// https://www.figma.com/design/zhShyVtHRyxfAubEfFNrbN/Untitled?node-id=1-3&t=nSV5CvmHh4N6JSnm-1
+import Lander from './Lander/Lander'
+
+
 
 function App() {
-  
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"></Route>
+        <Route path="/" element={<Lander />}></Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ amtapro_frontend/src/Lander/Lander.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './Hero';
+
+const Lander = () => {
+    return (
+        <div>
+            <Header />
+            <Hero />
+            <Footer />
+        </div>
+    );
+}
+
+export default Hero;
